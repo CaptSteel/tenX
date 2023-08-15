eb66c1241cc094b5f850c116d014d93ba09f432d
Shiva <shivasubramania24@gmail.com>
2023-08-15 15:28:04 +0530

    updates

--- basic/tenX.js
+++ basic/tenX.js
@@ -1,10 +1,48 @@
-var timerElement = $('#time');
-var testDiv = $('#test');
+var timeMinute = $('#timeMinute');
+var timeSecond = $('#timeSecond');
+var errorDiv = $('#error');
 
+$(document).ready(function () {
+  var minutes = 2;
+  var seconds = 0;
+  timeMinute.val(minutes);
+  timeSecond.val(seconds);
+});
 
-// preset values
-timerElement.val('00:01:00');
+$('#startTimer').click(function () {
+  minutes = Number( timeMinute.val());
+  seconds = Number(timeSecond.val());
+  countDownValidate();
+});
 
+function countDownValidate() {
+  if (minutes < 0) {
+    errorDiv.html('Minute cannot be negative! Error');
+    errorDiv.show();
+    return;
+  } else if (minutes > 60) {
+    errorDiv.html('Minute cannot be greater than 60! Error');
+    errorDiv.show();
+    return;
+  } else if (seconds < 0) {
+    errorDiv.html('Seconds cannot be negative! Error');
+    errorDiv.show();
+    return;
+  } else if (seconds > 60) {
+    errorDiv.html('Second cannot be greater than 60! Error');
+    errorDiv.show();
+    return;
+  }
+  countDownTimer();
+}
 
-
-console.log();
+function countDownTimer() {
+    var output = $('#output');
+    errorDiv.html("");
+    errorDiv.show();
+    var totalSeconds = minutes * 60 + seconds;
+    for (i=totalSeconds; i>=0; i--){
+        output.val(i);
+        console.log("Hi");
+    }
+}
